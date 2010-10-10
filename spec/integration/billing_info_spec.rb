78772199f8368e326c3ebea83b711362ce6e0e1a
Jacques Crocker <railsjedi@gmail.com>
2010-10-09 20:43:46 -0700

    Cleaning up Billing info specs

--- spec/integration/billing_info_spec.rb
+++ spec/integration/billing_info_spec.rb
@@ -18,7 +18,7 @@ module Recurly
       billing_info.credit_card.last_four.should == billing_attributes[:credit_card][:number]
     end
 
-    describe "#create" do
+    describe "create an account's billing information" do
       use_vcr_cassette "billing/create/#{timestamp}"
       let(:account){ Factory.create_account("billing-create-#{timestamp}") }
 
@@ -42,7 +42,28 @@ module Recurly
       end
     end
 
-    describe "#find" do
+    describe "update an account's billing information" do
+      use_vcr_cassette "billing/update/#{timestamp}"
+      let(:account){ Factory.create_account_with_billing_info("billing-update-#{timestamp}") }
+
+      before(:each) do
+        @new_billing_attributes = Factory.billing_attributes({
+          :account_code => account.account_code,
+          :first_name => account.first_name,
+          :last_name => account.last_name,
+          :address1 => "1st Ave South, Apt 5001"
+        })
+
+        @billing_info = BillingInfo.create(@new_billing_attributes)
+      end
+
+      it "should set the correct billing_info on the server " do
+        billing_info = BillingInfo.find(account.account_code)
+        verify_billing_info(billing_info, @new_billing_attributes)
+      end
+    end
+
+    describe "get account's billing information" do
       use_vcr_cassette "billing/find/#{timestamp}"
       let(:account){ Factory.create_account_with_billing_info("billing-find-#{timestamp}") }
 
@@ -62,19 +83,36 @@ module Recurly
       end
     end
 
-    describe "#destroy" do
+    describe "clearing an account's billing information" do
       use_vcr_cassette "billing/destroy/#{timestamp}"
-      let(:account){ Factory.create_account_with_billing_info("billing-destroy-#{timestamp}") }
+      let(:account){ Factory.create_account("billing-destroy-#{timestamp}") }
 
       before(:each) do
+        @billing_attributes = Factory.billing_attributes({
+          :account_code => account.account_code,
+          :first_name => account.first_name,
+          :last_name => account.last_name,
+          :address1 => "500 South Central Blvd",
+          :city => "Los Angeles",
+          :state => "CA",
+          :zip => "90001"
+        })
+
+        BillingInfo.create(@billing_attributes)
+
         @billing_info = BillingInfo.find(account.account_code)
       end
 
       it "should allow destroying the billing info for an account" do
         @billing_info.destroy
-        fresh = BillingInfo.find(account.account_code)
 
-        # TODO: check that the account's billing data was deleted
+        fresh = BillingInfo.find(account.account_code)
+        fresh.first_name.should be_nil
+        fresh.last_name.should be_nil
+        fresh.address1.should be_nil
+        fresh.city.should be_nil
+        fresh.state.should be_nil
+        fresh.zip.should be_nil
       end
     end
   end
