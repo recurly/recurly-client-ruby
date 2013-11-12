184fa175a99e887d194ef95aed5cde4b8f7556be
Chris Barton <chris.barton@recurly.com>
2013-11-11 17:50:37 -0800

    Version bump

--- lib/recurly/version.rb
+++ lib/recurly/version.rb
@@ -1,8 +1,8 @@
 module Recurly
   module Version
     MAJOR   = 2
-    MINOR   = 1
-    PATCH   = 9
+    MINOR   = 2
+    PATCH   = 0
     PRE     = nil
 
     VERSION = [MAJOR, MINOR, PATCH, PRE].compact.join('.').freeze
