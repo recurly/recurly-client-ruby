5646235fd6f3bebe37dd4d957a26ac8c5dd8cdb2
flahertyj <john.flaherty@dish.com>
2014-06-05 09:51:11 -0600

    Removed convert to xml for preview.

--- lib/recurly/resource.rb
+++ lib/recurly/resource.rb
@@ -778,7 +778,7 @@ module Recurly
     #   account.save # => true
     # @see #save!
     def preview
-        @response = API.send(:post, path + "/preview", to_xml(false))
+        @response = API.send(:post, path + "/preview"
     end
 
     # Attempts to save the record, returning +true+ if the record was saved and
