e5f61f44c8746e4214bd0f058b40ccc993d969fd
Adam Keys <adam@key-solutions.ca>
2014-08-25 19:05:26 -0400

    Remove dependence on external Account resource in notification tests

--- spec/recurly/webhook/notification_spec.rb
+++ spec/recurly/webhook/notification_spec.rb
@@ -2,48 +2,41 @@ require 'spec_helper'
 
 describe Webhook::Notification do
   let(:notification) {
+    klass = Class.new Resource do
+      attr_reader :reloaded
+    
+      def reload
+        @reloaded = true
+        self
+      end
+    end
+    Recurly.const_set :MockAccount, klass
+  
     Class.new Webhook::Notification do
-      has_one :account
+      has_one :mock_account
     end
   }
   
   describe ".has_one" do
     it "must define has_one from the Resource superclass" do
-      notification.new.must_respond_to :account
+      notification.new.must_respond_to :mock_account
     end
     
     it "must define has_one reload method" do
-      notification.new.must_respond_to :account!
+      notification.new.must_respond_to :mock_account!
     end
   end
   
   describe "has_one reload method" do
-    let(:account) { Account.new :account_code => 'code' }
-    let(:instance) { notification.new :account => account }
+    let(:instance) { notification.new :mock_account => MockAccount.new }
     
-    before do
-      stub_api_request :any, 'accounts/code', 'accounts/show-200'
+    it "must call reload on the object" do
+      account = instance.mock_account!
+      account.reloaded.must_equal true
     end
     
-    it "must find an account" do
-      instance.account!.must_be_instance_of Account
-    end
-    
-    it "must find the latest version" do
-      instance.account!.account_code.must_equal 'abcdef1234567890'
-    end
-    
-    it "wont return the existing account information" do
-      account = instance.account.dup
-      instance.account!.wont_equal account
-    end
-    
-    it "must update the existing account information" do
-      instance.account!.must_equal instance.account
-    end
-    
-    it "must handle missing accounts" do
-      notification.new.account!.must_be :nil?
+    it "must handle nil objects" do
+      notification.new.mock_account!.must_be_nil
     end
   end
 end
