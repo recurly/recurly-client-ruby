d4baeac1b49b6da15fbf5bc5e94e472e88d66db7
Benjamin Eckel <bhelx@simst.im>
2016-12-15 13:24:24 -0600

    Bump 2.8.0.rc1

--- lib/recurly/version.rb
+++ lib/recurly/version.rb
@@ -1,9 +1,9 @@
 module Recurly
   module Version
     MAJOR   = 2
-    MINOR   = 7
-    PATCH   = 6
-    PRE     = nil
+    MINOR   = 8
+    PATCH   = 0
+    PRE     = 'rc2'
 
     VERSION = [MAJOR, MINOR, PATCH, PRE].compact.join('.').freeze
 
