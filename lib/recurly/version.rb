7a15cc0a103e471c1a1d17b9ea201d1985dbc947
Benjamin Eckel <bhelx@simst.im>
2017-10-04 15:15:19 -0500

    Bump 2.11.0

--- lib/recurly/version.rb
+++ lib/recurly/version.rb
@@ -1,8 +1,8 @@
 module Recurly
   module Version
     MAJOR   = 2
-    MINOR   = 10
-    PATCH   = 2
+    MINOR   = 11
+    PATCH   = 0
     PRE     = nil
 
     VERSION = [MAJOR, MINOR, PATCH, PRE].compact.join('.').freeze
