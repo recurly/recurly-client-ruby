f3e826617e0a6ae4cfac227e250c61f0335ee171
andrew morton <drewish@katherinehouse.com>
2017-11-09 18:44:31 -0700

    Bump versions for 2.3.10

--- lib/recurly/version.rb
+++ lib/recurly/version.rb
@@ -2,7 +2,7 @@ module Recurly
   module Version
     MAJOR   = 2
     MINOR   = 3
-    PATCH   = 9
+    PATCH   = 10
     PRE     = nil
 
     VERSION = [MAJOR, MINOR, PATCH, PRE].compact.join('.').freeze
