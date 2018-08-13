8bf2cbf453a57abd71552429078fb25cfdad026f
Benjamin Eckel <bhelx@simst.im>
2018-08-13 18:22:17 -0500

    Net-http-persistent

--- lib/recurly/client.rb
+++ lib/recurly/client.rb
@@ -71,17 +71,14 @@ module Recurly
           faraday.response :logger
         end
         faraday.basic_auth(api_key, '')
-        faraday.adapter :net_http do |http| # yields Net::HTTP
+        faraday.adapter :net_http_persistent do |http| # yields Net::HTTP
           # Let's not use the bundled cert in production yet
           # but we will use these certs for any other staging or dev environment
           unless BASE_URL.end_with?('.recurly.com')
             http.ca_file = File.join(File.dirname(__FILE__), '../data/ca-certificates.crt')
           end
-          http.use_ssl = true
-          http.verify_mode = OpenSSL::SSL::VERIFY_PEER
           http.open_timeout = 50
           http.read_timeout = 60
-          http.keep_alive_timeout = 60
         end
       end
 
