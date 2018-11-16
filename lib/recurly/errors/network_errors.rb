3fcbc58de53d7985db5dd8472a60bfb7e77e7466
Benjamin Eckel <bhelx@simst.im>
2018-11-16 15:08:01 -0600

    More spec coverage

--- lib/recurly/errors/network_errors.rb
+++ lib/recurly/errors/network_errors.rb
@@ -2,7 +2,7 @@ module Recurly
   module Errors
     class NetworkError < StandardError; end
     class TimeoutError < NetworkError; end
-    class ConnectionFailed < NetworkError; end
+    class ConnectionFailedError < NetworkError; end
     class SSLError < NetworkError; end
   end
 end
---
+++ spec/recurly/errors_spec.rb
@@ -0,0 +1,19 @@
+require "spec_helper"
+require 'date'
+
+
+RSpec.describe Recurly::Errors::APIError do
+  describe ".error_class" do
+    error_keys = Recurly::Errors.constants - [:APIError]
+    error_keys = error_keys.map do |key|
+      key.to_s.split(/(?=[A-Z])/).map(&:downcase).join('_')
+    end
+
+    error_keys.each do |error_key|
+      it "should turn the error key #{error_key} into an error class" do
+        err_class = described_class.error_class(error_key)
+        expect(err_class.ancestors).to include(StandardError)
+      end
+    end
+  end
+end
