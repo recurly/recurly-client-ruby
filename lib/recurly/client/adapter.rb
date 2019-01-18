6d93e4c2f614a6359062a9c0096f5a96c7f61417
Benjamin Eckel <bhelx@simst.im>
2019-01-18 17:36:37 -0600

    Fix net-http-persistent keepalive

--- lib/recurly/client/adapter.rb
+++ lib/recurly/client/adapter.rb
@@ -7,7 +7,12 @@ module Recurly
       def configure_net_adapter(faraday)
         faraday.adapter :net_http_persistent do |http|
           # yields Net::HTTP::Persistent
-          http.keep_alive_timeout = 60
+          # for net-http-persistent 2.X, alternative is for 3.X
+          if http.respond_to? :keep_alive_timeout
+            http.keep_alive_timeout = 60
+          else
+            http.keep_alive = 60
+          end
         end
       end
     end
