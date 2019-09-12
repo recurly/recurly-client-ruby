c8f33bfe6697e4fccc73552b76aaff20175d8b82
Joanna Sese <jmgsese@gmail.com>
2019-09-12 14:46:56 -0500

    Add shipping_address return to spec file

--- spec/recurly/webhook/new_shipping_address_notification_spec.rb
+++ spec/recurly/webhook/new_shipping_address_notification_spec.rb
@@ -8,4 +8,10 @@ describe Webhook::NewShippingAddressNotification do
       notification.account.must_be_instance_of Account
     end
   end
+
+  describe "shipping_address" do
+    it "must return the shipping address" do
+      notification.shipping_address.must_be_instance_of ShippingAddress
+    end
+  end
 end
