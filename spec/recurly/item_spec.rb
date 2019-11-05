550bd93c20dbfb20a57057e1a4dc10c40894751b
Joanna Sese <jmgsese@gmail.com>
2019-11-05 16:58:17 -0600

    Add Item class Create Item class Add item_code attribute to Adjustment class Add unit tests

---
+++ spec/recurly/item_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper'
+
+describe Item do
+  let(:item) { Item.find 'plastic_gloves' }
+
+  describe ".find" do
+    it "must return an item when available" do
+      stub_api_request :get, 'items/plastic_gloves', 'items/show-200'
+
+      item.must_be_instance_of Item
+      item.description.must_equal 'Sleek Plastic'
+    end
+  end
+
+  describe "#save" do
+    before do
+      @item = Item.new
+    end
+
+    it "must return true when new and valid" do
+      stub_api_request :post, 'items', 'items/create-201'
+      @item.save.must_equal true
+      @item.description.must_equal 'Sleek Plastic'
+    end
+  end
+
+  describe "update" do
+    it "sends changed attributes to the server" do
+      stub_api_request :get, 'items/plastic_gloves', 'items/show-200'
+      stub_request(:put, "https://api.recurly.com/v2/items/plastic_gloves").
+      with(:body => "<item><name>Sleek Plastic Gloves</name></item>",
+        :headers => Recurly::API.headers).to_return(:status => 200, :body => "", :headers => {})
+      item.update_attributes({ name: 'Sleek Plastic Gloves' })
+    end
+  end
+
+  describe "custom fields" do
+    it "should have a custom field" do
+      stub_api_request :get, 'items/plastic_gloves', 'items/show-200'
+
+      item.custom_fields.must_equal [CustomField.new(name: 'color', value: 'blue')]
+    end
+
+    it "should update custom fields" do
+      stub_api_request :get, 'items/plastic_gloves', 'items/show-200'
+      
+      item.custom_fields.each do |custom_field|
+        custom_field.value="pink"
+      end
+      item.changed?.must_equal true
+    end
+  end
+
+  describe "delete" do
+    it "must delete an existing item" do
+      stub_api_request :get, 'items/plastic_gloves', 'items/show-200'
+      stub_api_request(:delete, 'items/plastic_gloves') { XML[200][:destroy] }
+      item.destroy.must_equal true
+    end
+  end
+
+  describe "reactivate" do
+    let (:inactive) {
+      stub_api_request(:get, 'items/inactive', 'items/show-200-inactive')
+      Item.find 'inactive'
+    }
+
+    it "must return true when deactivated account is reactivated" do
+      stub_api_request(:put, 'items/plastic_gloves/reactivate', 'items/show-200') 
+      inactive.reactivate.must_equal true
+    end
+  end
+
+end
