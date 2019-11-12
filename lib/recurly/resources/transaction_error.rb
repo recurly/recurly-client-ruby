05c0301bef8006c12100a057568e29cb692dc3b0
Benjamin Eckel <bhelx@simst.im>
2019-11-12 22:53:28 +0000

    Tue Nov 12 22:53:27 UTC 2019 Upgrade API version v2019-10-10

--- lib/recurly/resources/transaction_error.rb
+++ lib/recurly/resources/transaction_error.rb
@@ -22,6 +22,14 @@ module Recurly
       #   @return [String] Customer message
       define_attribute :message, String
 
+      # @!attribute object
+      #   @return [String] Object type
+      define_attribute :object, String
+
+      # @!attribute three_d_secure_action_token_id
+      #   @return [String] Returned when 3-D Secure authentication is required for a transaction. Pass this value to Recurly.js so it can continue the challenge flow.
+      define_attribute :three_d_secure_action_token_id, String
+
       # @!attribute transaction_id
       #   @return [String] Transaction ID
       define_attribute :transaction_id, String
