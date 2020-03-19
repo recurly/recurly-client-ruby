c3366d00673b34b0efc3bff10003390183848b15
Joanna Sese <jmgsese@gmail.com>
2020-03-19 17:53:01 -0500

    Add tier attributes to AddOn class and create Tier class

--- spec/recurly/add_on_spec.rb
+++ spec/recurly/add_on_spec.rb
@@ -6,7 +6,13 @@ describe AddOn do
       :get, 'plans/gold', 'plans/show-200'
     )
     stub_api_request(
+      :get, 'plans/plantfile', 'plans/show-200-tiered'
+    )
+    stub_api_request(
       :get, 'plans/gold/add_ons', 'plans/add_ons/index-200'
+    )
+    stub_api_request(
+      :get, 'plans/plantfile/add_ons', 'plans/add_ons/index-200-tiered'
     )
   end
 
@@ -19,6 +25,15 @@ describe AddOn do
 
       add_on = add_ons.first
       add_on.must_be_instance_of AddOn
+    end
+
+    it "must return an addon with tiered-pricing when available" do
+      plan = Plan.find 'plantfile'
+      add_ons = plan.add_ons
+
+      add_on = add_ons.first
+      add_on.must_be_instance_of AddOn
+      add_on.tier_type.must_equal "tiered"
     end
   end
 end
