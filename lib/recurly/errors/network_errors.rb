3e7d64aa21aeaf4e17befc08b65666ea9bc552fa
Doug Miller <doug@douglas-miller.com>
2020-06-26 13:23:52 -0500

    Updating errors

--- lib/recurly/errors/network_errors.rb
+++ lib/recurly/errors/network_errors.rb
@@ -6,5 +6,17 @@ module Recurly
     class ConnectionFailedError < NetworkError; end
     class SSLError < NetworkError; end
     class UnavailableError < NetworkError; end
+
+    # Missing generic 5XX -> UnavailableError
+    ERROR_MAP = {
+      "400" => "BadRequestError",
+      "401" => "UnauthorizedError",
+      "403" => "UnauthorizedError",
+      "408" => "TimeoutError",
+      "429" => "RateLimitedError",
+      "500" => "InternalServerError",
+      "503" => "UnavailableError",
+      "504" => "TimeoutError",
+    }
   end
 end
