03b57d2876bd4d0c3a4a5183c44a3c199eb28dad
DX Codefresh <dx@recurly.com>
2021-06-04 19:25:51 +0000

    Generated Latest Changes for v2021-02-25

--- lib/recurly/requests/billing_info_create.rb
+++ lib/recurly/requests/billing_info_create.rb
@@ -15,7 +15,7 @@ module Recurly
       define_attribute :amazon_billing_agreement_id, String
 
       # @!attribute backup_payment_method
-      #   @return [Boolean] The `backup_payment_method` indicator is used to designate a billing info as a backup on the account that will be tried if the billing info marked `primary_payment_method` fails. All payment methods, including the billing info marked `primary_payment_method` can be set as a backup. An account can have a maximum of 1 backup, if a user sets a different payment method as a backup, the existing backup will no longer be marked as such.
+      #   @return [Boolean] The `backup_payment_method` field is used to designate a billing info as a backup on the account that will be tried if the initial billing info used for an invoice is declined. All payment methods, including the billing info marked `primary_payment_method` can be set as a backup. An account can have a maximum of 1 backup, if a user sets a different payment method as a backup, the existing backup will no longer be marked as such.
       define_attribute :backup_payment_method, :Boolean
 
       # @!attribute company
@@ -67,7 +67,7 @@ module Recurly
       define_attribute :paypal_billing_agreement_id, String
 
       # @!attribute primary_payment_method
-      #   @return [Boolean] The `primary_payment_method` indicator is used to designate the primary billing info on the account. The first billing info created on an account will always become primary. Adding additional billing infos provides the flexibility to mark another billing info as primary, or adding additional non-primary billing infos. This can be accomplished by passing the `primary_payment_method` indicator. When adding billing infos via the billing_info and /accounts endpoints, this value is not permitted, and will return an error if provided.
+      #   @return [Boolean] The `primary_payment_method` field is used to designate the primary billing info on the account. The first billing info created on an account will always become primary. Adding additional billing infos provides the flexibility to mark another billing info as primary, or adding additional non-primary billing infos. This can be accomplished by passing the `primary_payment_method` with a value of `true`. When adding billing infos via the billing_info and /accounts endpoints, this value is not permitted, and will return an error if provided.
       define_attribute :primary_payment_method, :Boolean
 
       # @!attribute tax_identifier
--- lib/recurly/resources/billing_info.rb
+++ lib/recurly/resources/billing_info.rb
@@ -15,7 +15,7 @@ module Recurly
       define_attribute :address, :Address
 
       # @!attribute backup_payment_method
-      #   @return [Boolean] The `backup_payment_method` indicator is used to designate a billing info as a backup on the account that will be tried if the billing info marked `primary_payment_method` fails.
+      #   @return [Boolean] The `backup_payment_method` field is used to indicate a billing info as a backup on the account that will be tried if the initial billing info used for an invoice is declined.
       define_attribute :backup_payment_method, :Boolean
 
       # @!attribute company
@@ -51,7 +51,7 @@ module Recurly
       define_attribute :payment_method, :PaymentMethod
 
       # @!attribute primary_payment_method
-      #   @return [Boolean] The `primary_payment_method` indicator is used to designate the primary billing info on the account. The first billing info created on an account will always become primary. Adding additional billing infos provides the flexibility to mark another billing info as primary, or adding additional non-primary billing infos. This can be accomplished by passing the `primary_payment_method` indicator. When adding billing infos via the billing_info and /accounts endpoints, this value is not permitted, and will return an error if provided.
+      #   @return [Boolean] The `primary_payment_method` field is used to indicate the primary billing info on the account. The first billing info created on an account will always become primary. This payment method will be used
       define_attribute :primary_payment_method, :Boolean
 
       # @!attribute updated_at
