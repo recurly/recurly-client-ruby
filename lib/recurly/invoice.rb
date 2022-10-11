0634f760f40746f343c2bb344d3558fac042a37e
Niels Paulson-Thorp <nthorp@recurly.com>
2022-10-11 16:05:33 -0500

    add quantity_decimal to refunds

--- lib/recurly/invoice.rb
+++ lib/recurly/invoice.rb
@@ -278,7 +278,6 @@ module Recurly
       line_items ||= []
       node = builder.add_element 'line_items'
       line_items.each do |line_item|
-        binding.pry
         adj_node = node.add_element 'adjustment'
         adj_node.add_element 'uuid', line_item[:adjustment].uuid
         adj_node.add_element 'quantity', line_item[:quantity]
