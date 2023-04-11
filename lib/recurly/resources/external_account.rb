960ba93ce0ffe52b0603c84e626738f600fe0a8b
DX Codefresh <dx@recurly.com>
2023-04-11 19:05:52 +0000

    Generated Latest Changes for v2021-02-25

---
+++ lib/recurly/resources/external_account.rb
@@ -0,0 +1,26 @@
+# This file is automatically created by Recurly's OpenAPI generation process
+# and thus any edits you make by hand will be lost. If you wish to make a
+# change to this file, please create a Github issue explaining the changes you
+# need and we will usher them to the appropriate places.
+module Recurly
+  module Resources
+    class ExternalAccount < Resource
+
+      # @!attribute created_at
+      #   @return [DateTime] Created at
+      define_attribute :created_at, DateTime
+
+      # @!attribute external_account_code
+      #   @return [String] Represents the account code for the external account.
+      define_attribute :external_account_code, String
+
+      # @!attribute external_connection_type
+      #   @return [String] Represents the connection type. `AppleAppStore` or `GooglePlayStore`
+      define_attribute :external_connection_type, String
+
+      # @!attribute updated_at
+      #   @return [DateTime] Last updated at
+      define_attribute :updated_at, DateTime
+    end
+  end
+end
