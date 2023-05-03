9538a7ef5d6ba576d402b0eabb20d9d46c5b89c0
DX Codefresh <dx@recurly.com>
2023-05-03 18:39:40 +0000

    Generated Latest Changes for v2019-10-10

--- lib/recurly/resources/tax_detail.rb
+++ lib/recurly/resources/tax_detail.rb
@@ -6,12 +6,24 @@ module Recurly
   module Resources
     class TaxDetail < Resource
 
+      # @!attribute billable
+      #   @return [Boolean] Whether or not the line item is taxable. Only populated for a single LineItem fetch when Avalara for Communications is enabled.
+      define_attribute :billable, :Boolean
+
+      # @!attribute level
+      #   @return [String] Provides the jurisdiction level for the Communications tax applied. Example values include city, state and federal. Present only when Avalara for Communications is enabled.
+      define_attribute :level, String
+
+      # @!attribute name
+      #   @return [String] Provides the name of the Communications tax applied. Present only when Avalara for Communications is enabled.
+      define_attribute :name, String
+
       # @!attribute rate
       #   @return [Float] Provides the tax rate for the region.
       define_attribute :rate, Float
 
       # @!attribute region
-      #   @return [String] Provides the tax region applied on an invoice. For Canadian Sales Tax, this will be either the 2 letter province code or country code.
+      #   @return [String] Provides the tax region applied on an invoice. For Canadian Sales Tax, this will be either the 2 letter province code or country code. Not present when Avalara for Communications is enabled.
       define_attribute :region, String
 
       # @!attribute tax
@@ -19,7 +31,7 @@ module Recurly
       define_attribute :tax, Float
 
       # @!attribute type
-      #   @return [String] Provides the tax type for the region. For Canadian Sales Tax, this will be GST, HST, QST or PST.
+      #   @return [String] Provides the tax type for the region or type of Comminications tax when Avalara for Communications is enabled. For Canadian Sales Tax, this will be GST, HST, QST or PST.
       define_attribute :type, String
     end
   end
