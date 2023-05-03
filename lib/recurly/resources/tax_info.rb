9019cd08941c37462035076b84f5b18f0a9143e5
DX Codefresh <dx@recurly.com>
2023-05-03 19:36:39 +0000

    Generated Latest Changes for v2021-02-25

--- lib/recurly/resources/tax_info.rb
+++ lib/recurly/resources/tax_info.rb
@@ -7,19 +7,19 @@ module Recurly
     class TaxInfo < Resource
 
       # @!attribute rate
-      #   @return [Float] Rate
+      #   @return [Float] The combined tax rate. Not present when Avalara for Communications is enabled.
       define_attribute :rate, Float
 
       # @!attribute region
-      #   @return [String] Provides the tax region applied on an invoice. For U.S. Sales Tax, this will be the 2 letter state code. For EU VAT this will be the 2 letter country code. For all country level tax types, this will display the regional tax, like VAT, GST, or PST.
+      #   @return [String] Provides the tax region applied on an invoice. For U.S. Sales Tax, this will be the 2 letter state code. For EU VAT this will be the 2 letter country code. For all country level tax types, this will display the regional tax, like VAT, GST, or PST. Not present when Avalara for Communications is enabled.
       define_attribute :region, String
 
       # @!attribute tax_details
-      #   @return [Array[TaxDetail]] Provides additional tax details for Canadian Sales Tax when there is tax applied at both the country and province levels. This will only be populated for the Invoice response when fetching a single invoice and not for the InvoiceList or LineItem.
+      #   @return [Array[TaxDetail]] Provides additional tax details for Communications taxes when Avalara for Communications is enabled or Canadian Sales Tax when there is tax applied at both the country and province levels. This will only be populated for the Invoice response when fetching a single invoice and not for the InvoiceList or LineItemList. Only populated for a single LineItem fetch when Avalara for Communications is enabled.
       define_attribute :tax_details, Array, { :item_type => :TaxDetail }
 
       # @!attribute type
-      #   @return [String] Provides the tax type as "vat" for EU VAT, "usst" for U.S. Sales Tax, or the 2 letter country code for country level tax types like Canada, Australia, New Zealand, Israel, and all non-EU European countries.
+      #   @return [String] Provides the tax type as "vat" for EU VAT, "usst" for U.S. Sales Tax, or the 2 letter country code for country level tax types like Canada, Australia, New Zealand, Israel, and all non-EU European countries. Not present when Avalara for Communications is enabled.
       define_attribute :type, String
     end
   end
