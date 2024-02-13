68e9bc2a969ffe5cdf4a19d904d76b1260c39ce8
Seth Brown <seth.brown@recurly.com>
2024-02-13 14:51:03 -0500

    add revrec to shipping methods

--- spec/recurly/shipping_method_spec.rb
+++ spec/recurly/shipping_method_spec.rb
@@ -2,12 +2,21 @@ require 'spec_helper'
 
 describe ShippingMethod do
   describe ".find" do
-    it "must return a shipping method when available" do
+    before do
       stub_api_request(
         :get, 'shipping_methods/fedex_ground', 'shipping_methods/show-200'
-      )
+        )
+    end
+    it "must return a shipping method when available" do
       shipping_method = ShippingMethod.find 'fedex_ground'
       shipping_method.must_be_instance_of ShippingMethod
+    end
+
+    it "returns RevRec values" do
+      shipping_method = ShippingMethod.find 'fedex_ground'
+      shipping_method.performance_obligation_id.must_equal('4')
+      shipping_method.revenue_gl_account_id.must_equal('thproqnpcuwp')
+      shipping_method.liability_gl_account_id.must_equal('twywqfr48v9l')
     end
   end
 end
