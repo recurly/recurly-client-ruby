6d60dd6f985f207509dd68098c9588298df26f55
Recurly Integrations <dx@recurly.com>
2024-04-30 18:13:44 +0000

    Generated Latest Changes for v2021-02-25

--- lib/recurly/requests/invoice_create.rb
+++ lib/recurly/requests/invoice_create.rb
@@ -27,7 +27,7 @@ module Recurly
       define_attribute :net_terms, Integer
 
       # @!attribute net_terms_type
-      #   @return [String] Optionally supplied string that may be either `net` or `eom` (end-of-month). When `net`, an invoice becomes past due the specified number of `Net Terms` days from the current date. When `eom` an invoice becomes past due the specified number of `Net Terms` days from the last day of the current month.  This field is only available when the EOM Net Terms feature is enabled.
+      #   @return [String] Optionally supplied string that may be either `net` or `eom` (end-of-month). When `net`, an invoice becomes past due the specified number of `Net Terms` days from the current date. When `eom` an invoice becomes past due the specified number of `Net Terms` days from the last day of the current month.
       define_attribute :net_terms_type, String
 
       # @!attribute po_number
